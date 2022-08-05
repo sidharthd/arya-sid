b18ac72a08cc9375d865a13fd9667bd8391818b0
Sidharth D <sidharth@firstbytedigital.com>
2022-08-05 22:43:04 +0530

    Add Head component

---
+++ components/Head.tsx
@@ -0,0 +1,9 @@
+import NextHead from "next/head";
+
+export const Head = () => (
+  <NextHead>
+    <title>Arya and Sidharth</title>
+    <meta name="description" content="Personal website of Arya and Sidharth" />
+    <link rel="icon" href="/favicon.ico" />
+  </NextHead>
+);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+
+import { Head } from "components";
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>Arya and Sidharth</title>
-        <meta
-          name="description"
-          content="Personal website of Arya and Sidharth"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <Head />
 
       <main>
         <h1>Arya & Sidharth</h1>
