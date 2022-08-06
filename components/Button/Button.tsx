95865c09af635e745360520d627598ec9cd2a337
Sidharth D <sidharth@firstbytedigital.com>
2022-08-07 00:03:31 +0530

    Add button target

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -14,7 +14,7 @@ export const Button: React.FC<ButtonProps> = ({
   <button
     onClick={onClick}
     type="button"
-    className={`text-black hover:text-white border-2 border-pink-500 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-pink-300 font-bold rounded-lg px-5 py-2.5 text-center mr-2 mb-2 dark:border-pink-500 dark:text-pink-500 dark:hover:text-white dark:hover:bg-pink-600 dark:focus:ring-pink-800 ${className}`}
+    className={`text-black hover:text-white border-2 border-pink-500 hover:bg-pink-500 focus:ring-4 focus:outline-none focus:ring-pink-300 font-bold rounded-lg px-5 py-2.5 text-center mr-2 mb-2 dark:border-pink-500 dark:text-pink-500 dark:hover:text-white dark:hover:bg-pink-600 dark:focus:ring-pink-800 transition duration-500 ${className}`}
   >
     {children}
   </button>
--- components/Hero/HeroCTA.tsx
+++ components/Hero/HeroCTA.tsx
@@ -1,10 +1,18 @@
+import Link from "next/link";
 import { Button } from "components";
+
+const weddingLocation = "https://goo.gl/maps/C5hJjDiuGfSrth4E9";
+const receptionLocation = "https://goo.gl/maps/RDFdikRrGrwpby2R9";
 
 export const HeroCTA = () => {
   return (
     <div className="mt-10 space-y-3">
-      <Button className="w-full">View wedding venue on map</Button>
-      <Button className="w-full">View reception venue on map</Button>
+      <a href={weddingLocation} target="_blank" rel="noreferrer">
+        <Button className="w-full">View wedding venue on map</Button>
+      </a>
+      <a href={receptionLocation} target="_blank" rel="noreferrer">
+        <Button className="w-full">View reception venue on map</Button>
+      </a>
     </div>
   );
 };
